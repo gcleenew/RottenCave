22a54b8b44665e94fec06719d7e80e4c935c35f4
prossato <prossato@scapiniere>
2015-10-13 12:20:33 +0200

    Generate libgdx RottenCave project

---
+++ android/src/org/isep/rottencave/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package org.isep.rottencave.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import org.isep.rottencave.RottenCave;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new RottenCave(), config);
+	}
+}
